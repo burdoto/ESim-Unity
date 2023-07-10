dd8bca2f93f76d4e90518cc04bd105482564640c
burdoto <kaleidox@comroid.org>
2023-07-10 16:08:27 +0200

    models

--- Assets/Scripts/Model.cs
+++ Assets/Scripts/Model.cs
@@ -1,4 +1,18 @@
-﻿public interface IEComponent
+﻿using JetBrains.Annotations;
+using UnityEngine;
+
+public interface IEComponent
 {
     EComponent.Types ComponentType { get; }
+}
+
+public abstract class EComponent : MonoBehaviour, IEComponent
+{
+    [CanBeNull] public Wire Input;
+    [CanBeNull] public Wire Output;
+    public byte State;
+    
+    public abstract Types ComponentType { get; }
+    
+    public enum Types { Conductor, Switch, Consumer, Source }
 }
